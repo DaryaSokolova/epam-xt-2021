b9a3105668cfc1867f715ae62f6d43c1a40b042f
Дарья Соколова <sklv.d01@gmail.com>
2021-03-16 21:01:31 +0400

    add CompareTo

--- Task 2/task 2.1/MyStringClass.cs
+++ Task 2/task 2.1/MyStringClass.cs
@@ -6,5 +6,70 @@ namespace task_2._1
 {
     class MyStringClass
     {
+
+        private char[] chars;
+        public MyStringClass(string str)
+        {
+            chars = str.ToCharArray();
+        }
+
+        public void writeChars()
+        {
+            foreach (char ch in chars)
+            {
+
+                Console.WriteLine(ch);
+            }
+        }
+
+        public int Length
+        {
+            get
+            {
+                return chars.Length;
+            }
+        }
+
+        public int CompareTo(MyStringClass strB)
+        {
+
+            if (this > strB)
+            {
+                return 1;
+            }
+
+            if (this < strB)
+            {
+                return -1;
+            }
+
+            return 0;
+        }
+        public static bool operator >(MyStringClass c1, MyStringClass c2)
+        {
+            for (int i = 0; i < c1.Length; i++)
+            {
+                if (c1.chars[i] != c2.chars[i])
+                {
+                    return c1.chars[i] > c2.chars[i];
+                }
+
+            }
+
+            return false;
+        }
+        public static bool operator <(MyStringClass c1, MyStringClass c2)
+        {
+            for (int i = 0; i < c1.Length; i++)
+            {
+                if (c1.chars[i] != c2.chars[i])
+                {
+                    return c1.chars[i] < c2.chars[i];
+                }
+
+            }
+
+            return false;
+        }
     }
 }
--- Task 2/task 2.1/Program.cs
+++ Task 2/task 2.1/Program.cs
@@ -7,6 +7,16 @@ namespace task_2._1
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            string str1 = "Hello World, I love you!";
+            string str2 = "Hello Darya!";
+
+            MyStringClass newString1 = new MyStringClass(str1);
+            MyStringClass newString2 = new MyStringClass(str2);
+
+            Console.WriteLine(str1.CompareTo(str2));
+
+            Console.WriteLine(newString1.CompareTo(newString2));
         }
     }
 }
