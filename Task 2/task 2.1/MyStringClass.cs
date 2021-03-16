0db47f5885756fcaf82aec45ca71596d3add9c1a
Дарья Соколова <sklv.d01@gmail.com>
2021-03-16 21:54:50 +0400

    add Concat

--- Task 2/task 2.1/MyStringClass.cs
+++ Task 2/task 2.1/MyStringClass.cs
@@ -8,18 +8,20 @@ namespace task_2._1
     {
 
         private char[] chars;
+
+        public MyStringClass(char[] _chars)
+        {
+            chars = _chars;
+        }
+
         public MyStringClass(string str)
         {
             chars = str.ToCharArray();
         }
 
-        public void writeChars()
+        public char[] getChars()
         {
-            foreach (char ch in chars)
-            {
-
-                Console.WriteLine(ch);
-            }
+            return chars;
         }
 
         public int Length
@@ -45,6 +47,7 @@ namespace task_2._1
 
             return 0;
         }
+
         public static bool operator >(MyStringClass c1, MyStringClass c2)
         {
             for (int i = 0; i < c1.Length; i++)
@@ -58,6 +61,7 @@ namespace task_2._1
 
             return false;
         }
+
         public static bool operator <(MyStringClass c1, MyStringClass c2)
         {
             for (int i = 0; i < c1.Length; i++)
@@ -70,6 +74,32 @@ namespace task_2._1
             }
 
             return false;
+        }
+
+        public static char[] operator +(MyStringClass c1, MyStringClass c2)
+        {
+            char[] temp = new char[c1.Length+c2.Length];
+
+            for (int i = 0; i < c1.Length ; i++)
+            {
+                temp[i] = c1.chars[i];
+            }
+
+            int tempCount = 0;
+
+            for (int i = c1.Length; i < c1.Length + c2.Length; i++)
+            {
+                temp[i] = c2.chars[tempCount++];
+            }
+
+            return temp;
+        }
+
+        public static MyStringClass Concat(MyStringClass strA, MyStringClass strB)
+        {
+            MyStringClass str = new MyStringClass(strA + strB);
+
+            return str;
         }
     }
 }
--- Task 2/task 2.1/Program.cs
+++ Task 2/task 2.1/Program.cs
@@ -6,17 +6,24 @@ namespace task_2._1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Console.WriteLine("Hello World!");
 
-            string str1 = "Hello World, I love you!";
-            string str2 = "Hello Darya!";
+            string str1 = "Hello World!";
+            string str2 = "Hello Darya Sokolova!";
 
             MyStringClass newString1 = new MyStringClass(str1);
             MyStringClass newString2 = new MyStringClass(str2);
 
-            Console.WriteLine(str1.CompareTo(str2));
+            //Console.WriteLine(str1.CompareTo(str2));
+            //Console.WriteLine(newString1.CompareTo(newString2));
 
-            Console.WriteLine(newString1.CompareTo(newString2));
+            //str1 = String.Concat(str1, str2);
+            //Console.WriteLine(str1);
+
+            //newString1= MyStringClass.Concat(newString1, newString2);
+            //Console.WriteLine(newString1.getChars());
+
+
         }
     }
 }
