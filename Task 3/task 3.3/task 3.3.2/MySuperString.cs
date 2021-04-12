ec96edec1e87db680ae67a8d1b9985d4dacbc276
Дарья Соколова <sklv.d01@gmail.com>
2021-04-12 19:26:22 +0400

    complete 3.3.2

---
+++ Task 3/task 3.3/task 3.3.2/MySuperString.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace task_3._3._2
+{
+    public static class MySuperString
+    {
+        public static string WhatLanguage(this string str)
+        {
+            char[] chars = str.ToCharArray();
+
+            bool engString = chars.All(ch => (ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z'));
+            bool rusString = chars.All(ch => (ch >= 'а' && ch <= 'я') || (ch >= 'А' && ch <= 'Я'));
+            bool numString = chars.All(ch => (ch >= '0' && ch <= '9'));
+
+            if(engString == true && rusString == false && numString == false)
+            {
+                return "English";
+            }
+
+            if (engString == false && rusString == true && numString == false)
+            {
+                return "Russian";
+            }
+
+            if (engString == false && rusString == false && numString == true)
+            {
+                return "Numbers";
+            }
+
+            return "Mixed";
+        }
+
+
+    }
+}
--- Task 3/task 3.3/task 3.3.2/Program.cs
+++ Task 3/task 3.3/task 3.3.2/Program.cs
@@ -7,6 +7,10 @@ namespace task_3._3._2
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            string str = "рол4 5678";
+
+            Console.WriteLine(str.WhatLanguage());
         }
     }
 }
